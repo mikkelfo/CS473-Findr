1ba83f3318e13cbb4b955efe60c06a1ca34f9199
mikkelfo <mikk528a@gmail.com>
2019-11-05 12:06:10 +0900

    Initialization

---
+++ src/main/java/Findr/Cs473FindrApplication.java
@@ -0,0 +1,13 @@
+package Findr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Cs473FindrApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Cs473FindrApplication.class, args);
+	}
+
+}
---
+++ src/test/java/Findr/Cs473FindrApplicationTests.java
@@ -0,0 +1,13 @@
+package Findr;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Cs473FindrApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
